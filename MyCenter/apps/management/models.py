a7da9d70a8f9b696aa9c3eb41d4a9940f691678f
unknown <554092287@qq.com>
2018-12-25 18:32:56 +0800

    ready for update

--- MyCenter/apps/management/models.py
+++ MyCenter/apps/management/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 # Create your models here.
+
+
+class Staff(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
--- MyCenter/apps/management/views.py
+++ MyCenter/apps/management/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.contrib.admin.views.decorators import staff_member_required
 # Create your views here.
+
+
+
+
+
