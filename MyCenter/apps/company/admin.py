cda7bbffb2b7b566e498e89eeeb0ac03b0c0a4cb
hzr <1245241827@qq.com>
2018-12-26 09:38:07 +0800

    企业模型搭建和后台数据添加

--- MyCenter/apps/company/admin.py
+++ MyCenter/apps/company/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from company.models import CompanyType,Company_content
+
+admin.site.site_header = '后台企业管理平台'
+@admin.register(CompanyType)
+class CompanyTypeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Company_content)
+class Company_contentAdmin(admin.ModelAdmin):
+    pass
--- MyCenter/apps/company/views.py
+++ MyCenter/apps/company/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
+from company.models import CompanyType,Company_content
 
-# Create your views here.
+class LoginView(View):
+    def get(self,request):
+        pass
+
+    def post(self,request):
+        pass
+
+class IndexView(View):
+    def get(self,request):
+        Company_content.objects.filter()
+
+
+
+    def post(self,request):
+        pass
