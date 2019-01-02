159d74749e5d2ada8b42c661cd845b7334b0a566
unknown <554092287@qq.com>
2019-01-02 16:58:00 +0800

    add product search view

--- MyCenter/apps/product/urls.py
+++ MyCenter/apps/product/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-from product.views import CategoryView
+from product.views import product_search
 
 urlpatterns = [
-    url(r'^category/(?P<cate_id>\d+)/$', CategoryView.as_view(), name='CategoryView')
+    
 ]
